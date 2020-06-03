1c0807556794f4c9165da4fc5a1cd2c54c0dbad6
Tonodera1 <t.onodera.1.2.1.5@gmail.com>
2020-06-03 22:31:34 +0900

    サインイン・登録のビュー完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <p><router-link to="/signin">サインイン</router-link></p>
+    <p><router-link to="/register">登録</router-link></p>
     <router-view/>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import SignIn from './views/SignIn'
-import Register from './views/Register'
+import SignIn from './views/SignIn.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router)
 
@@ -16,9 +16,17 @@ export default new Router({
       component: Home
     },
     {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
       path: '/register',
       name: 'register',
-      components: Register
+      component: Register
     },
     {
       path: '/signin',
