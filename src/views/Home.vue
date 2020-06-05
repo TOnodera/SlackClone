bafa5fd81c48ea5a54cfa4257684c11c742b53d1
Tonodera1 <t.onodera.1.2.1.5@gmail.com>
2020-06-05 22:30:50 +0900

    ヘッダー迄追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,93 @@
 <template>
-  <div>
-    <h1>Slack Clone</h1>
-    <p>ログイン中</p>
-    <div>
-      <button class="py-1 px-4 bg-gray-800 text-white rouded" @click="signOut">サインアウト</button>
+  <div class="flex h-screen">
+    <div class="w-1/5 bg-gray-800 text-white pt-3 px-4">
+      <div class="flex justify-between items-center">
+        <h1 class="font-semibold text-xl leading-tight">Slack Clone</h1>
+        <Notification/>
+      </div>
+      <div class="flex items-center">
+         <span class="bg-yellow-400 rounded-full w-3 h-3 mr-2"></span>
+         <span class="opacity-50">{{user.email}}</span>
+      </div>
+      <div class="mt-5 flex justify-between items-center">
+        <div class="font-bold opacity-50 text-lg">
+          <div
+          class="opacity-50 mt-1"
+          v-for="channel in channels"
+          :key="channel.id">#{{channel.channel_name}}</div>
+        </div>
+        <PlusCircle/>
+       </div>
+       <div class="mt-5 flex justify-between items-center">
+          <div class="font-bold opacity-50 text-lg">ダイレクトメッセージ</div>
+          <PlusCircle />
+        </div>
+        <div class="mt-2 flex items-center" v-for="user in users" :key="user.user_id">
+          <span class="bg-yellow-400 rounded-full w-3 h-3 mr-2"></span>
+          <span class="opacity-50">{{ user.email }}</span>
+        </div>
+    </div>
+    <div class="flex-grow bg-gray-100">
+      <header class="border-b">
+        <div class="flex justify-between m-3">
+          <div class="font-bold text-lg">{{ user.email}}</div>
+          <div>
+            <button class="py-1 px-4 bg-gray-800 text-white rounded" @click="singOut">サインアウト</button>
+          </div>
+        </div>
+      </header>
     </div>
   </div>
 </template>
 <script>
 import firebase from 'firebase/app'
 import 'firebase/database'
+import Notification from '../components/icons/Notification.vue'
+import PlusCircle from '../components/icons/PlusCircle.vue'
 
 export default {
   name: 'Home',
+  components:{
+    Notification,
+    PlusCircle
+  },
   data() {
     return {
-      message: '',
-      messages:[]
+      user: '',
+      users: [
+        {
+          user_id: 11,
+          email: "john@example.com"
+        },
+        {
+          user_id: 12,
+          email: "kevin@test.com"
+        },
+        {
+          user_id: 13,
+          email: "susan@test.com"
+        }
+      ],
+      channels:[
+        {
+          id:1,
+          channel_name: '営業'
+        },
+        {
+          id:2,
+          channel_name: 'マーケティング'
+        },
+        {
+          id:3,
+          channel_name: '情シス'
+        }
+      ]
     }
   },
   mounted() {
-    firebase.database().ref('slack')
-      .on('value',snapshot =>{this.messages=snapshot.val()})
+    this.user = firebase.auth().currentUser
   },
   methods:{
-    addMessage() {
-      firebase.database().ref('slack')
-        .push({
-          content: this.message,
-          user: {
-            name: 'Jhon Doe'
-          }
-        })
-    },
-    deleteMessage(index) {
-      firebase
-        .database()
-        .ref('slack')
-        .child(index)
-        .remove()
-    },
     signOut() {
       firebase.auth().signOut()
       this.$router.push('/signin')
