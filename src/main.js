ccf216b16e897d9146bc2468e1faa448c1e57632
TOnodera <46294684+TOnodera@users.noreply.github.com>
2020-06-09 22:40:45 +0900

    apikeyを削除

--- src/main.js
+++ src/main.js
@@ -9,13 +9,7 @@ Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyCbobrdb_YEx4xsQeCcQmdKZArSy302lWM',
-  authDomain: 'vue-slack-clone-6853a.firebaseapp.com',
-  databaseURL: 'https://vue-slack-clone-6853a.firebaseio.com',
-  projectId: 'vue-slack-clone-6853a',
-  storageBucket: 'vue-slack-clone-6853a.appspot.com',
-  messagingSenderId: '494335862169',
-  appId: '1:494335862169:web:3b464beefecaafc5699f08'
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
