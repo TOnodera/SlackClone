c004e311e2c9b42de8a5a1e65a5a29314da1b94f
Tonodera1 <t.onodera.1.2.1.5@gmail.com>
2020-06-09 19:43:05 +0900

    チャンネルに送受信できるところまで実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,11 +18,11 @@
           v-show="channelModal"
           @click="closeChannelModal"
         >
-          <div class="z-20 bg-white text-gray-900 w-1/3 rounded-md">
+          <div class="z-20 bg-white text-gray-900 w-1/3 rounded-md" v-on:click.stop>
             <div class="flex flex-col p-6">
               <div class="flex justify-between items-center">
                 <h2 class="text-3xl font-black leading-loose">チャンネルを作成する</h2>
-                <span class="text-4xl">×</span>
+                <span class="text-4xl" @click="closeChannelModal">×</span>
               </div>
               <p>チャンネルはチームがコニュニケーションを取る場所です。特定のトピックに基づいてチャンネルを作ると良いでしょう。(例: #マーケティング)</p>
               <div class="mt-8 font-semibold">名前</div>
@@ -30,11 +30,13 @@
                 <input
                   type="text"
                   class="w-full rounded border-gray-900 border-solid border p-3"
+                  v-model="channel"
                 />
               </div>
               <div class="flex justify-end">
                 <button
                   class="px-8 py-2 rounded bg-green-900 font-bold text-white"
+                  @click="addChannel"
                 >作成</button>
               </div>
             </div>
@@ -46,6 +48,7 @@
           class="opacity-50 mt-1"
           v-for="channel in channels"
           :key="channel.id"
+          @click="channelMessage(channel)"
         ># {{ channel.channel_name }}
       </div>
        <div class="mt-5 flex justify-between items-center">
@@ -141,22 +144,10 @@ export default {
       message: '',
       messages: [],
       placeholder: '',
-      channels: [
-        {
-          id: 1,
-          channel_name: "営業"
-        },
-        {
-          id: 2,
-          channel_name: "マーケティング"
-        },
-        {
-          id: 3,
-          channel_name: "情シス"
-        }
-      ],
+      channels: [],
       channel_id: '',
-      channelModal:false
+      channelModal: false,
+      channel: ''
     }
   },
   mounted () {
@@ -169,7 +160,14 @@ export default {
       .on('child_added',snapshot=>{
         this.users.push(snapshot.val())
       })
-
+    
+    firebase
+      .database()
+      .ref('channel')
+      .on('child_added',(snapshot)=>{
+        this.channels.push(snapshot.val())
+      })
+    
   },
   beforeDestroy(){
     firebase
@@ -238,6 +236,50 @@ export default {
     },
     closeChannelModal() {
       this.channelModal = false
+    },
+    addChannel() {
+
+      const newChannel = firebase
+            .database()
+            .ref('channel')
+            .push()
+
+      const key_id = newChannel.key
+
+      newChannel
+        .set({
+          channel_name: this.channel,
+          id: key_id
+        })
+        .then(()=>{
+          this.channelModal = false
+        })
+
+      this.channel = ''
+
+    },
+    channelMessage(channel) {
+
+      this.messages = []
+      this.channel_name = '# ' + channel.channel_name
+      this.channel_id = channel.id
+
+      if(this.channel_id!=''){
+        firebase
+          .database()
+          .ref('messages')
+          .child(this.channel_id)
+          .off()
+      }
+
+      firebase
+        .database()
+        .ref('messages')
+        .child(this.channel_id)
+        .on('child_added',(snapshot)=>{
+          this.messages.push(snapshot.val())
+        })
+
     }
   }
 }
